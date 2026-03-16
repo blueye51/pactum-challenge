efc5b366f51aac0d0719360301a916b2d20825b6
Eric Rand <eric.rand66@gmail.com>
2026-03-16 16:26:16 +0200

    initial spring boot and vite react ts

---
+++ backend/pactum_challange/src/main/java/com/pareto/pactum_challange/PactumChallangeApplication.java
@@ -0,0 +1,13 @@
+package com.pareto.pactum_challange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PactumChallangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PactumChallangeApplication.class, args);
+	}
+
+}
---
+++ backend/pactum_challange/src/test/java/com/pareto/pactum_challange/PactumChallangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pareto.pactum_challange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PactumChallangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
