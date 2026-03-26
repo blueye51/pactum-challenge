70953795749440c2bef1f9267bfb0583ad433161
Eric Rand <eric.rand66@gmail.com>
2026-03-26 14:23:21 +0200

    frontend deployement fix

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
+echo "BACKEND_URL=${BACKEND_URL}"
+echo "PORT=${PORT}"
+
+if [ -z "$BACKEND_URL" ]; then
+  echo "ERROR: BACKEND_URL is not set"
+  exit 1
+fi
+
+if [ -z "$PORT" ]; then
+  PORT=80
+fi
+
 envsubst '${BACKEND_URL} ${PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/conf.d/default.conf
+cat /etc/nginx/conf.d/default.conf
 exec nginx -g 'daemon off;'
